9920f3ce9ef45a8009e8a5d2853208db4057a6f1
Radim Mifka <radimmifka@gmail.com>
2021-01-06 23:33:06 +0100

    errors output

--- Scraper.py
+++ Scraper.py
@@ -4,11 +4,12 @@ import pandas as pd
 import pathlib
 import json
 import time
+import os
 
 
 class Scraper:
 
-    def __init__(self, products_filename = "products.json", import_filename="fields.json",export_filename = "export.csv", separator=";", fields_file = True, test=False, login=False, login_data={}, login_url=""):
+    def __init__(self, products_filename="products.json", import_filename="fields.json", export_filename="export.csv", separator=";", fields_file=True, test=False, login=False, login_data={}, login_url=""):
         self.base_path = str(pathlib.Path(__file__).parent.absolute())
         self.test = test
         self.get_products(products_filename)
@@ -42,27 +43,39 @@ class Scraper:
         else:
             return requests.get(url).content
 
-    def export(self, data):
+    def export(self, data, errors):
         data = pd.DataFrame.from_records(data)
-        data.to_csv(f"{self.base_path}/{self.export_filename}",sep=f"{self.separator}")
+        data.to_csv(
+            f"{self.base_path}/output/{self.export_filename}", sep=f"{self.separator}")
+        with open(f'{self.base_path}/output/errors.json', 'w') as f:
+            json.dump(errors, f)
+        print("Process finished --- %s seconds ---" %
+              (time.time() - self.start_time))
 
     def scrape(self, tree):
         data = {}
         for el in self.wanted_fields:
             val = None
             if el["type"] == "str":
-                if len(el["split"][0])>0:
+                if len(el["split"][0]) > 0:
                     for split in el["split"]:
                         if val is None:
-                            val = self.clean(self.clean("".join(tree.xpath(el["xpath"] + "//text()"))).split(split[0])[split[1]])
+                            val = self.clean(self.clean(
+                                "".join(tree.xpath(el["xpath"] + "//text()"))).split(split[0])[split[1]])
                         else:
                             val = self.clean(val.split(split[0])[split[1]])
                 else:
-                    val = self.clean("".join(tree.xpath(el["xpath"] + "//text()")))
+                    val = self.clean(
+                        "".join(tree.xpath(el["xpath"] + "//text()")))
             elif el["type"] == "links":
-                val = self.list_to_string(self.remove_duplicates(tree.xpath(el["xpath"] + "/@href")))
+                val = self.list_to_string(self.remove_duplicates(
+                    tree.xpath(el["xpath"] + "/@href")))
             elif el["type"] == "imgs":
-                val = self.list_to_string(self.remove_duplicates(tree.xpath(el["xpath"] + "/@src")))
+                val = self.list_to_string(self.remove_duplicates(
+                    tree.xpath(el["xpath"] + "/@src")))
+            elif el["type"] == "cat":
+                val = self.list_to_string(tree.xpath(
+                    el["xpath"] + "//text()"), delimiter="/")
             else:
                 val = ""
             data[el["name"]] = val
@@ -75,15 +88,15 @@ class Scraper:
 
         if "\n" in bloated_string:
             if bloated_string.startswith("\n"):
-                bloated_string.replace("\n","")
-            bloated_string.replace("\n","<br>")
+                bloated_string.replace("\n", "")
+            bloated_string.replace("\n", "<br>")
         return " ".join(bloated_string.strip().split())
 
     def remove_duplicates(self, bloated_list: list) -> list:
         return list(set(bloated_list))
 
-    def list_to_string(self, bloated_list: list) -> str:
-        return ",".join(bloated_list)
+    def list_to_string(self, bloated_list, delimiter=",") -> str:
+        return delimiter.join(bloated_list)
 
     def set_fields(self):
         if self.fields_file:
@@ -96,7 +109,7 @@ class Scraper:
                 self.wanted_fields.append({
                     "name": input("Zadej nazev: "),
                     "type": input("Zadej typ [str, links, list, imgs]: "),
-                    "xpath":input("Zadej xpath: ")
+                    "xpath": input("Zadej xpath: ")
                 })
                 action = int(input("Akce: "))
                 if action == 1:
@@ -106,21 +119,23 @@ class Scraper:
 
     def run(self):
         self.set_fields()
+        self.start_time = time.time()
         data = []
+        errors = []
         for i, product in enumerate(self.products):
             if i == 0:
                 start_time = time.time()
-            print("Průběh {:2.1%}, Zbývající čas: {} min".format(i / len(self.products),round(self.one_time*len(self.products[i:len(self.products)])/60),0), end="\r")
+            print("Průběh {:2.1%}, Zbývající čas: {} min".format(i / len(self.products),
+                                                                 round(self.one_time*len(self.products[i:len(self.products)])/60), 0), end="\r")
             tree = html.fromstring(self.get(product))
-            data.append(self.scrape(tree))
+            try:
+                data.append(self.scrape(tree))
+            except:
+                errors.append(product)
+
             if i == 0:
                 self.one_time = time.time() - start_time
-        self.export(data)
+        self.export(data, errors)
 
-login_data = {
-"login":"kocmazoo@protonmail.com",
-"pass":"baraaiwa",
-"button":"submit"
-}
 
-Scraper(test=True, login=True, login_url="https://b2b.jkanimals.cz/cs/uzivatel/ucet/login/", login_data=login_data).run()
+Scraper(test=True, login=True).run()
